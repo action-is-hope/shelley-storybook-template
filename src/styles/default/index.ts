53012b515e7928de66b2a36138b67828d3aa8de2
mnapthine <mnapthine@gmail.com>
2023-03-23 16:01:10 +0000

    Initial first port.

---
+++ src/styles/default/index.ts
@@ -0,0 +1,10 @@
+// = Bundle 'ALL' () Global
+import "./allGlobal.st.css";
+
+import "./button.st.css";
+import "./text.st.css";
+
+// = Project
+import { classes as ShelleyDefaultProject } from "./project.st.css";
+
+export const Project = ShelleyDefaultProject.root;
---
+++ src/styles/shelley/index.ts
@@ -0,0 +1,17 @@
+// = Foundation Components
+import "./button.st.css";
+import "./text.st.css";
+
+// = Project
+import { classes as shelleyProject } from "./project.st.css";
+
+// = Themes
+import { classes as light } from "./themes/light.st.css";
+import { classes as dark } from "./themes/dark.st.css";
+
+// = Main classname export
+export const Project = shelleyProject.root;
+
+// = Theme exports
+export const Light = light.root;
+export const Dark = dark.root;
